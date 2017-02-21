71d9e6bc94da6668efb1b5c834b13475bf1973de
Paolo Basso <paolo.basso87@gmail.com>
2017-02-21 18:04:07 +0100

    Fixed image-webpack-loader warnings

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,18 @@ module.exports = {
                 test:    /\.(jpe?g|png|gif|svg)$/i,
                 loaders: [
                     'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-                    'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                    {
+                        loader: 'image-webpack-loader',
+                        query: {
+                            bypassOnDebug: false,
+                            gifsicle: {
+                                interlaced: false
+                            },
+                            optipng: {
+                                optimizationLevel: 7
+                            }
+                        }
+                    }
                 ]
             }
         ],
